27406f061a8c524c4eceab4a4779ed10251c2769
LautnerDD <lautner1116@gmail.com>
2019-06-24 15:46:43 +0800

    63

---
+++ 63-uniquePathsII/63-uniquePathsII.cpp
@@ -0,0 +1,15 @@
+class Solution {
+public:
+    int uniquePathsWithObstacles(vector<vector<int>>& obstacleGrid) {
+        int m=obstacleGrid.size(),n=obstacleGrid[0].size();
+        vector<long> res(n,0);
+        res[0]=1;
+        for(int i=0;i<m;i++){
+            for(int j=0;j<n;j++){
+                if(obstacleGrid[i][j]==1)res[j]=0;
+                else if(j>0)res[j]+=res[j-1];
+            }
+        }
+        return res[n-1];
+    }
+};
