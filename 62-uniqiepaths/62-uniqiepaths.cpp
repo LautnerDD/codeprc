931ffa9782aca9ed0809fd57b403f32efbf1214c
LautnerDD <lautner1116@gmail.com>
2019-06-24 11:44:24 +0800

    62

---
+++ 62-uniqiepaths/62-uniqiepaths.cpp
@@ -0,0 +1,13 @@
+class Solution {
+public:
+    int uniquePaths(int m, int n) {
+        vector<int> res(m,1);
+        for(int i=1;i<n;i++){
+            for(int j=1;j<m;j++){
+                res[j]+=res[j-1];
+            }
+        }
+        return res[m-1];
+        
+    }
+};
